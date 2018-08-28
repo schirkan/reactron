e8c4d25f8c2302259366a711d21209fd552cc608
Martin Pietschmann <schirkan86@gmx.de>
2018-08-28 16:57:47 +0200

    remove react-loadable register electron in SystemJS

--- src/ModuleLoader.tsx
+++ src/ModuleLoader.tsx
@@ -4,7 +4,6 @@ import * as FontAwesome from '@fortawesome/react-fontawesome';
 import * as path from 'path';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import * as Loadable from 'react-loadable';
 import { ExternalModule } from './ExternalModule';
 
 const electron = (window as any).require('electron') as Electron.AllElectron;
@@ -15,27 +14,12 @@ SystemJS.config({
   map: { 'plugin-json': 'node_modules/systemjs-plugin-json/json.js' },
   meta: { '*.json': { loader: 'plugin-json' } }
 });
+SystemJS.set('electron', SystemJS.newModule(electron));
 SystemJS.set('react', SystemJS.newModule(React));
 SystemJS.set('react-dom', SystemJS.newModule(ReactDom));
 SystemJS.set('@fortawesome/fontawesome-svg-core', SystemJS.newModule(SvgCore));
 SystemJS.set('@fortawesome/free-solid-svg-icons', SystemJS.newModule(SvgIcons));
 SystemJS.set('@fortawesome/react-fontawesome', SystemJS.newModule(FontAwesome));
-
-export const loadComponent = <TProps extends object = any, TExports extends object = any>(from: string, component: string = 'default') => Loadable<TProps, TExports>({
-  loader: () => SystemJS.import(from).then((m: any) => {
-    console.log(from, m);
-    return m[component];
-  }),
-  loading: (props: any) => {
-    if (props.error) {
-      return <div>Error! <button onClick={props.retry}>Retry</button></div>;
-    } else if (props.pastDelay) {
-      return <div>Loading...</div>;
-    } else {
-      return null;
-    }
-  },
-});
 
 export const loadModule = async (folderName: string): Promise<ExternalModule> => {
   const packageFile = path.join('modules', folderName, 'package.json');
